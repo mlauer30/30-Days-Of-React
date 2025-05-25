3fdce429531517774574de5325b1da9a32ce3f10
mlauer30 <mlauer07@gmail.com>
2025-05-25 14:51:34 -0400

    (complete) Exercise3-2

--- 01_Day_JavaScript_Refresher/exercises3/exercise2.js
+++ 01_Day_JavaScript_Refresher/exercises3/exercise2.js
@@ -86,8 +86,79 @@ function statsCounter( obj ) {
 }
 
 console.log(statsCounter( users ));
+/*
+1. Find people who are MERN stack developer from the users object
+*/
 
+function getMernDevelopers( obj ) {
+    let mernDevs = [];
+    for (let [key, value] of Object.entries(obj)) {
+        if(value.skills.includes("MongoDB", "Express", "React", "Node")) {
+            mernDevs.push(key);        
+        } 
+    }
+    return mernDevs;
+}
+console.log(getMernDevelopers(users));
+/*
+1. Set your name in the users object without modifying the original users object
+*/
 
+let copyOfUsers = {
+    ...users,
+    Matthew: {
+        email: 'matthew@matthew.com',
+        skills: ["Java", "Pyhton", "JavaScript", "React"],
+        age: 28,
+        isLoggedIn: false,
+        points: 50
+    },
+    
+};
+
+console.log(copyOfUsers.Matthew);
+
+/*
+1. Get all keys or properties of users object
+*/
+
+console.log(Object.keys(users));
+
+/*
+1. Get all the values of users object
+*/
+
+console.log(Object.values(users));
+
+/*
+1. Use the countries object to print a country name, capital, populations and languages.
+*/
+
+let countries = {
+    1: {
+        name: "Brazil",
+        capital: "Brasilia",
+        population: 220000000,
+        languages: ["Portugese", "Spanish", "English", "Xavante", "Other"], 
+    },
+    2: {
+        name: "United States of America",
+        captial: "Washington, District of Columbia",
+        population: 300000000,
+        languages: ["English", "Spanish", "Other"],
+    }
+}
+function getCountryValues( obj, country ) {
+    let countryData = {};
+    for(let [key, value] of Object.entries(obj)) {
+        if(value.name === country) {
+            countryData = value;
+        }
+    } 
+    return countryData;
+}
+console.log(getCountryValues(countries, "Brazil"));
+console.log(getCountryValues(countries, "United States of America"));
 
 
 
